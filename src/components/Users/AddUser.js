1d91464fe1cb91ba424635a788143bcf7ccafd66
Rohit Sharma <codingscenes@gmail.com>
2022-09-28 17:44:11 +0530

    Update AddUser.js

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,26 +1,36 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import Button from '../UIElements/Button';
 import Card from '../UIElements/Card';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
-  const [enteredName, setEnteredName] = useState('');
-  const [enteredMob, setEnteredMob] = useState('');
+  // const [enteredName, setEnteredName] = useState('');
+  // const [enteredMob, setEnteredMob] = useState('');
+  // scoped. will not affect other component
+  const useNameRef = useRef(); //only in curren component
+  const useMobileRef = useRef();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    props.onAddUser(enteredName, enteredMob);
-    setEnteredName('');
-    setEnteredMob('');
+    const username = useNameRef.current.value;
+    const mobileNo = useMobileRef.current.value;
+
+    console.dir(document.getElementById('username'));
+    props.onAddUser(username, mobileNo);
+    console.log(useNameRef);
+    useNameRef.current.value = '';
+    useMobileRef.current.value = '';
+    // setEnteredName('');
+    // setEnteredMob('');
   };
 
   const nameChangeHandler = (event) => {
-    setEnteredName(event.target.value);
+    // setEnteredName(event.target.value);
   };
 
   const mobChangeHandler = (event) => {
-    setEnteredMob(event.target.value);
+    // setEnteredMob(event.target.value);
   };
 
   return (
@@ -31,15 +41,15 @@ const AddUser = (props) => {
           <input
             id='username'
             type='text'
-            value={enteredName}
             onChange={nameChangeHandler}
+            ref={useNameRef}
           />
           <label htmlFor='mobile Number'>Mobile Number</label>
           <input
             id='mobileNo'
             type='number'
-            value={enteredMob}
             onChange={mobChangeHandler}
+            ref={useMobileRef}
           />
           <Button type='submit'>ADD</Button>
         </form>
