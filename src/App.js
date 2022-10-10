8b9add3ba918b09a544dad28e950934eecaa1782
Rohit Sharma <codingscenes@gmail.com>
2022-10-10 19:03:07 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Input from './UI/Input';
-
 const App = () => {
   const [fullName, setFullName] = useState('');
+  const childInputRef = useRef(null);
 
   const changeHandler = (event) => {
     const value = event.target.value;
     setFullName(value);
   };
+
+  useEffect(() => {
+    console.log(childInputRef);
+    childInputRef.current.focusChildInput();
+  }, []);
 
   return (
     <div className='main'>
@@ -21,6 +26,7 @@ const App = () => {
           label={'Full Name'}
           invalid={false}
           onInputChange={changeHandler}
+          ref={childInputRef}
         />
       </div>
     </div>
--- src/UI/Input.js
+++ src/UI/Input.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { forwardRef, useImperativeHandle, useRef } from 'react';
 
-const Input = (props) => {
+const Input = forwardRef((props, ref) => {
+  const inputRef = useRef(null);
+
+  const focusInput = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      // key: value
+      focusChildInput: focusInput,
+      myName: 'Rohit Sharma',
+    };
+  });
+
   return (
     <div className='form-group'>
       <label htmlFor={props.label}>{props.label}</label>
       <input
+        className='form-control'
         type={props.type}
         name={props.name}
         value={props.value}
-        className={`form-control ${props.invalid && 'invalid'} `}
         onChange={props.onInputChange}
         autoComplete={'off'}
+        ref={inputRef}
       />
     </div>
   );
-};
+});
 
 export default Input;
