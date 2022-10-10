b6690e17a8211ac691cd043f9394248d61c508d0
Rohit Sharma <codingscenes@gmail.com>
2022-10-10 13:20:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,52 +4,24 @@ import React, { useState } from 'react';
 import Input from './UI/Input';
 
 const App = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [invalidEmail, setInvalidEmail] = useState(false);
-  const [invalidPassword, setInvalidPassword] = useState(false);
+  const [fullName, setFullName] = useState('');
 
-  const submitHandler = (event) => {
-    event.preventDefault();
-    if (!email) {
-      setInvalidEmail(true);
-    } else if (!password) {
-      setInvalidPassword(true);
-    } else {
-      console.log('Form is ready to  submit');
-    }
-  };
   const changeHandler = (event) => {
-    const inputName = event.target.name;
     const value = event.target.value;
-    if (inputName === 'email') setEmail(value);
-    if (inputName === 'password') setPassword(value);
+    setFullName(value);
   };
 
   return (
     <div className='main'>
       <div className='card'>
-        <form onSubmit={submitHandler}>
-          <Input
-            type={'text'}
-            name={'email'}
-            value={email}
-            label={'Email ID'}
-            invalid={invalidEmail}
-            onInputChange={changeHandler}
-          />
-          <Input
-            type={'password'}
-            name={'password'}
-            value={password}
-            label={'Password'}
-            invalid={invalidEmail}
-            onInputChange={changeHandler}
-          />
-          <button className='btn btn-success' type='submit'>
-            Login
-          </button>
-        </form>
+        <Input
+          type={'text'}
+          name={'fullName'}
+          value={fullName}
+          label={'Full Name'}
+          invalid={false}
+          onInputChange={changeHandler}
+        />
       </div>
     </div>
   );
