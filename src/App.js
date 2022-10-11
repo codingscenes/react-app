c09f14002580aaf883817ade587465cb574d74c1
Rohit Sharma <codingscenes@gmail.com>
2022-10-11 14:46:53 +0530

    ....

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useRef, useState } from 'react';
-import Input from './UI/Input';
+import React from 'react';
 const App = () => {
-  const [fullName, setFullName] = useState('');
-  const childInputRef = useRef(null);
-
-  const changeHandler = (event) => {
-    const value = event.target.value;
-    setFullName(value);
-  };
-
-  useEffect(() => {
-    console.log(childInputRef);
-    childInputRef.current.focusChildInput();
-  }, []);
-
   return (
-    <div className='main'>
-      <div className='card'>
-        <Input
-          type={'text'}
-          name={'fullName'}
-          value={fullName}
-          label={'Full Name'}
-          invalid={false}
-          onInputChange={changeHandler}
-          ref={childInputRef}
-        />
-      </div>
+    <div className='App'>
+      <h1>Hello, World!</h1>
     </div>
   );
 };
