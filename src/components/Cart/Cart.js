3a4cb1e0b3f4f8c5e66785ce1d3ee63c5dec3afe
Coding Scenes <codingscenes@gmail.com>
2022-11-04 00:47:08 +0530

    next we will work on complex reducer logic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,26 @@
+import { useContext } from 'react';
+import CartContext from './../../store/cart-context';
 import Modal from './../../UI/Modal';
 import classes from './Cart.module.css';
+import CartItem from './CartItem';
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `₹${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[
-        {
-          id: 'c1',
-          name: 'Black Tea',
-          amount: 2,
-          price: 222,
-        },
-      ].map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -22,13 +30,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>433</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-              <button className={classes['button--alt']}
-              onClick={props.onClose}
-              >Close</button>
-        <button className={classes.button}>Order</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
