06529bd4c0ebb6b6743e1034910ead7cba117819
Coding Scenes <codingscenes@gmail.com>
2022-11-20 00:55:30 +0530

    ...

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,18 +2,19 @@ import { useDispatch, useSelector } from 'react-redux';
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
 
   const dispatch = useDispatch();
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
 
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
   };
 
   const increaseHandler = () => {
-    // sending extra data or payload on action object
-    // it wil sent to reducer function and data can be accessed on action object
     dispatch({ type: 'increase', value: 20 });
   };
 
@@ -24,7 +25,7 @@ const Counter = () => {
   return (
     <main className='counter'>
       <h1>Redux Counter</h1>
-      <div className='value'> {counter}</div>
+      {showCounter && <div className='value'> {counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increment By 20</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,33 @@
 import { createStore } from 'redux';
 
-const initialState = { counter: 0 };
+const initialState = { counter: 0, showCounter: true };
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === 'increase') {
     return {
       counter: state.counter + action.value,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === 'decrement') {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === 'toggle') {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
     };
   }
 
