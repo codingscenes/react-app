1650170f23f6423b746fcd86f8559a2e5b158de2
Coding Scenes <codingscenes@gmail.com>
2022-11-19 15:54:53 +0530

    actions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,32 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Counter = () => {
+  // state change refreshed value
+  // setting subscription with state
+  //new changes will be reflected on counter variable
   const counter = useSelector((state) => state.counter);
 
+  const dispatch = useDispatch();
+
   const toggleCounterHandler = () => {};
+
+  const incrementHandler = () => {
+    // dispatching action
+    dispatch({ type: 'increment' });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  };
 
   return (
     <main className='counter'>
       <h1>Redux Counter</h1>
       <div className='value'> {counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import { createStore } from 'redux';
 const initialState = { counter: 0 };
 
 const counterReducer = (state = initialState, action) => {
+
   if (action.type === 'increment') {
     return {
       counter: state.counter + 1,
