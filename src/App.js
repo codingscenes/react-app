58b0c448b60699998e0d9c8fdb54c2607ec81090
Coding Scenes <codingscenes@gmail.com>
2022-11-23 19:21:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
+let isInitial = true;
+
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    const sendCartData = async () => {
+      const response = await fetch(
+        'https://react-learning-project-6b928-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Failed to send cart data...');
+      }
+      const responseData = await response.json();
+    };
+
+    sendCartData().catch((error) => {
+      console.log('[ERROR]', error.message);
+    });
+  }, [cart]);
+
+  useSelector((state) => state.cart);
   return (
     <Layout>
       {showCart && <Cart />}
