4adfc0cf5ce4801301595d09d90c4276e54eb7fd
Coding Scenes <codingscenes@gmail.com>
2022-11-27 01:53:04 +0530

    ..

---
+++ src/pages/AddComments.js
@@ -0,0 +1,10 @@
+const AddComments = () => {
+  return (
+    <section>
+      <textarea rows={4} cols={4}></textarea>
+      <button>Submit</button>
+    </section>
+  );
+};
+
+export default AddComments;
--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,4 +1,5 @@
 import { Link, Route, useParams, useRouteMatch } from 'react-router-dom';
+import AddComments from './AddComments';
 import ProductComments from './ProductComments';
 
 const ProductDetails = () => {
@@ -13,7 +14,8 @@ const ProductDetails = () => {
     <div>
       <Route path={match.path} exact>
         <h2>This is product details.</h2>
-        <Link to={`${match.url}/ProductComments`}>Check Comments</Link>
+        <Link to={`${match.url}/ProductComments`}>Read Comments</Link> ||
+        <Link to={`${match.url}/AddComments`}>Add Comments</Link>
       </Route>
       <p>{productId === 'p1' && 'This is  about Mobile details'}</p>
       <p>{productId === 'p2' && 'This is  about Laptop details'}</p>
@@ -21,6 +23,9 @@ const ProductDetails = () => {
 
       <Route path={`${match.path}/ProductComments`}>
         <ProductComments />
+      </Route>
+      <Route path={`${match.path}/AddComments`}>
+        <AddComments />
       </Route>
     </div>
   );
