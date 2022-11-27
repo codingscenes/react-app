6be8bc9db6cf898e7fe24ba6647a98c7c90cfded
Coding Scenes <codingscenes@gmail.com>
2022-11-28 00:49:53 +0530

    ...

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -3,32 +3,36 @@ import AddComments from './AddComments';
 import ProductComments from './ProductComments';
 
 const dummyDetails = {
-  p1: 'This is  about Mobile details',
-  p2: 'This is  about Laptop details',
-  p3: 'This is  about Tablet details',
+  p1: ['APPLE', 'HUAWEI', 'XIAOMI', 'OPPO', 'LENOVO', 'LG', 'NOKIA'],
+  p2: ['APPLE MAC', 'Samsung', 'Dell', 'ASUS'],
+  p3: ['APPLE', 'XIAOMI', 'LENOVO', 'LG'],
 };
 const ProductDetails = () => {
   const params = useParams();
 
-  const match = useRouteMatch();
-
   const productId = params.productId;
 
-  return (
-    <div>
-      <Route path={match.path} exact>
-        <h2>This is product details.</h2>
-        <Link to={`${match.url}/ProductComments`}>Read Comments</Link> ||
-        <Link to={`${match.url}/AddComments`}>Add Comments</Link>
-      </Route>
-      <p>{dummyDetails[productId]}</p>
+  let isSortingAsc = true;
 
-      <Route path={`${match.path}/ProductComments`}>
-        <ProductComments />
-      </Route>
-      <Route path={`${match.path}/AddComments`}>
-        <AddComments />
-      </Route>
+  const sortAscFn = (a, b) => a.localeCompare(b);
+
+  const sortDescFn = (a, b) => b.localeCompare(a);
+
+  const list = dummyDetails[productId].sort(
+    isSortingAsc ? sortAscFn : sortDescFn
+  );
+
+  const changeSortingHandler = () => { };
+  
+  return (
+    <div className='details'>
+      <button onClick={changeSortingHandler}>Sort Ascending</button>
+      <h2>This is product details.</h2>
+      <ul>
+        {list.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 };
