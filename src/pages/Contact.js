4cfc1e2437caf25f705a4953ce96251b11ae037c
Coding Scenes <codingscenes@gmail.com>
2022-11-28 01:52:45 +0530

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,18 @@
+import { useRef } from 'react';
+
 const Contact = () => {
+  const messageRef = useRef('');
+
   const onSubmitHandler = (event) => {
     event.preventDefault();
+    const message = messageRef.current.value;
   };
   return (
     <form onSubmit={onSubmitHandler}>
       <div className='ui form '>
         <div className='field'>
           <label>Write your message:</label>
-          <textarea rows='2'></textarea>
+          <textarea rows='2' ref={messageRef}></textarea>
         </div>
         <button className='positive ui button'>Submit</button>
       </div>
