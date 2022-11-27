dc37e68844000f879101a09204dcffcd512f00c6
Coding Scenes <codingscenes@gmail.com>
2022-11-28 02:19:39 +0530

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,22 +1,51 @@
-import { useRef } from 'react';
+import { Fragment, useRef, useState } from 'react';
+
+import { Prompt, useHistory } from 'react-router-dom';
 
 const Contact = () => {
   const messageRef = useRef('');
+  const history = useHistory();
+
+  const [iAmTyping, setIAmTyping] = useState(false);
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
     const message = messageRef.current.value;
+    messageRef.current.value = '';
+    history.push('/welcome');
+  };
+
+  const formFocusHandler = (event) => {
+    setIAmTyping(true);
+  };
+
+  const setIAmTypingFalseHandler = () => {
+    setIAmTyping(false);
   };
   return (
-    <form onSubmit={onSubmitHandler}>
-      <div className='ui form '>
-        <div className='field'>
-          <label>Write your message:</label>
-          <textarea rows='2' ref={messageRef}></textarea>
+    <Fragment>
+      <Prompt
+        when={iAmTyping}
+        message={(location) =>
+          'Are you sure, you want to leave? All your data will be lost!'
+        }
+      />
+
+      <form onFocus={formFocusHandler} onSubmit={onSubmitHandler}>
+        <div className='ui form '>
+          <div className='field'>
+            <label>Write your message:</label>
+            <textarea rows='2' ref={messageRef}></textarea>
+          </div>
+          <button
+            className='positive ui button'
+            onClick={setIAmTypingFalseHandler}
+          >
+            Submit
+          </button>
         </div>
-        <button className='positive ui button'>Submit</button>
-      </div>
-    </form>
+      </form>
+    </Fragment>
   );
 };
 
