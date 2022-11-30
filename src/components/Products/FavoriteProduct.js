564a8c4787288c31ceb72c780ebf725293928bb5
Coding Scenes <codingscenes@gmail.com>
2022-11-30 20:28:36 +0530

    Update FavoriteProduct.js

--- src/components/Products/FavoriteProduct.js
+++ src/components/Products/FavoriteProduct.js
@@ -5,30 +5,36 @@ const PRODUCT_DATA = [
     id: 'p1',
     title: 'Black Tea',
     description: 'De-stree yourself with yummy black tea',
-    isFavorite: false,
+    isFavorite: true,
   },
   {
     id: 'p2',
     title: 'Black Coffee',
     description: 'Rest yourself with yummy black Coffee',
-    isFavorite: false,
+    isFavorite: true,
   },
 ];
 const FavoriteProduct = () => {
   const clickedItemHandler = () => {};
+  const filteredProduct = PRODUCT_DATA.filter((p) => p.isFavorite);
   return (
     <Layout>
       <div className='ui segment'>
         <div className='ui list'>
-          {PRODUCT_DATA.map((item) => (
+          {filteredProduct.map((item) => (
             <ProductItem
               key={item.id}
+              id={item.id}
               title={item.title}
               description={item.description}
               isFavorite={item.isFavorite}
-              clikedItem={clickedItemHandler}
             />
           ))}
+          {!filteredProduct.length && (
+            <div className='item'>
+              <h2>No favorite product available!</h2>
+            </div>
+          )}
         </div>
       </div>
     </Layout>
