e2e5546d8e10a9f9483d530f06958937658c0c44
Coding Scenes <codingscenes@gmail.com>
2022-11-30 20:34:19 +0530

    ...

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import configureStore from './hooks-store/product-store';
 import './index.css';
-
-configureStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
