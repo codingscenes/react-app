29a0a24ba39922291c9292936a422f55d623e1e9
Coding Scenes <codingscenes@gmail.com>
2023-04-14 20:03:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import { withState } from './withState';
+import React, { useState } from 'react';
 
-function App({ count, increment, decrement }) {
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(prev => prev + 1);
+  };
+
+  const decrement = () => {
+    setCount(prev => prev - 1);
+  };
+
   return (
     <div>
       <h2>Counter: {count}</h2>
@@ -11,5 +20,5 @@ function App({ count, increment, decrement }) {
   );
 }
 
-export default withState(App);
+export default App;
 
