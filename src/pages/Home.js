f3efd77b17968a434ba51d3c4df55a2e33e06dfc
Coding Scenes <codingscenes@gmail.com>
2023-09-23 23:23:10 +0530

    ....

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <a href='/users'>Users</a>
+    </div>
+  );
 };
 export default HomePage;
