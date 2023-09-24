18a2d6469903ac95dc4be82a080abd43d8b33902
Coding Scenes <codingscenes@gmail.com>
2023-09-24 17:18:16 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,18 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const loginHandler = () => {
+    navigate('/users', { replace: true});
+  };
+
   return (
     <div>
       <h1>HomePage</h1>
       <Link to='/users'>Users</Link>
+      <hr />
+      <button onClick={loginHandler}>Login</button>
     </div>
   );
 };
