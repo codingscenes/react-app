87dcccf9d6079cc3fea556583b7f365c1e63bf6b
Coding Scenes <codingscenes@gmail.com>
2023-09-24 12:30:00 +0530

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
+import MainNavigation from '../components/MainNavigation';
 
 const HomePage = () => {
   return (
     <div>
+      <MainNavigation />
       <h1>HomePage</h1>
-      <a href='/users'>Users</a>
+      <Link to='/users'>Users</Link>
     </div>
   );
 };
