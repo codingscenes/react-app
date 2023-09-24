d838aaed1766d42de73c7b0070deb22e9259a19a
Coding Scenes <codingscenes@gmail.com>
2023-09-24 16:54:27 +0530

    .

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,13 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import styles from './MainNavigation.module.css';
+
 const MainNavigation = () => {
   return (
     <header>
-      <ul className='navigation'>
+      <ul className={styles.navigation}>
         <li>
-          <Link to='/'>Home</Link>
+          <NavLink to='/' className={({ isActive }) => (isActive ? styles.active : undefined)}>
+            Home
+          </NavLink>
         </li>
         <li>
-          <Link to='/users'>Users</Link>
+          <NavLink to='/users' className={({ isActive }) => (isActive ? styles.active : undefined)} end>
+            Users
+          </NavLink>
         </li>
       </ul>
     </header>
